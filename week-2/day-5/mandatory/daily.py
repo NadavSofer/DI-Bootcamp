16d0f212b61ef9e684b2d08a6a511e1b0cfa474e
NadavSofer <nadavsof5@gmail.com>
2023-03-30 15:50:53 +0300

    week-2 day-5

--- week-2/day-5/mandatory/daily.py
+++ week-2/day-5/mandatory/daily.py
@@ -2,7 +2,6 @@ string = 'without,hello,bag,world'
 
 str_split = string.split(',')
 
-new_list = [word for word in str_split] #----------------------------------->
 list_sorted = sorted(str_split)
 
 string_sorted = ', '.join(list_sorted)
