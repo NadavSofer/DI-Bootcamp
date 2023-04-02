1c2db59c2ef4a7bdbfef920ef2b5f97fbac0e8f0
NadavSofer <nadavsof5@gmail.com>
2023-04-02 14:58:22 +0300

    add daily

--- week-3/day-1/mandatory.py
+++ week-3/day-1/mandatory.py
@@ -62,66 +62,103 @@
 
 # Exercise 4 : Afternoon At The Zoo ------------------------------------------
 
-class Zoo:
-
-    
-    def __init__(self, zoo_name) -> None:
-        self.zoo_name = zoo_name
-        self.animals = []
-        self.animals_dict = {}
+# class Zoo:
 
 
-    def add_animal(self, new_animal):
-        if new_animal not in self.animals:
-            self.animals.append(new_animal)
-        else:
-            print('animal is already in the list')
-        
-    
-    def get_animals(self):
-        print(self.animals)
+#     def __init__(self, zoo_name) -> None:
+#         self.zoo_name = zoo_name
+#         self.animals = []
+#         self.animals_dict = {}
 
 
-    def sell_animal(self, sold_animal):
-        if sold_animal in self.animals:
-            self.animals.remove(sold_animal)
-        else:
-            print('animal is not in the list')
+#     def add_animal(self, new_animal):
+#         if new_animal not in self.animals:
+#             self.animals.append(new_animal)
+#         else:
+#             print('animal is already in the list')
 
-    def sort_animals(self):
-        sorted(self.animals)
-        for animal in self.animals:
-            char = animal[0]
-            if char in self.animals_dict:
-                self.animals_dict[char].append(animal)
-            else:
-                self.animals_dict[char] = [animal]
-        
 
-    def get_groups(self):
-        print(self.animals_dict)
+#     def get_animals(self):
+#         print(self.animals)
 
-zoo = Zoo('thing')
 
-zoo.add_animal('tiger')
-zoo.add_animal('toger')
-zoo.add_animal('Bear')
-zoo.add_animal('Ape')
-zoo.add_animal('Baboon')
-zoo.add_animal('Cougar')
-zoo.add_animal('Eel')
-zoo.add_animal('Emu')
-zoo.add_animal('snake')
+#     def sell_animal(self, sold_animal):
+#         if sold_animal in self.animals:
+#             self.animals.remove(sold_animal)
+#         else:
+#             print('animal is not in the list')
 
-zoo.sell_animal('toger')
+#     def sort_animals(self):
+#         sorted(self.animals)
+#         for animal in self.animals:
+#             char = animal[0]
+#             if char in self.animals_dict:
+#                 self.animals_dict[char].append(animal)
+#             else:
+#                 self.animals_dict[char] = [animal]
 
-zoo.sort_animals()
 
-zoo.get_animals()
+#     def get_groups(self):
+#         print(self.animals_dict)
+
+# zoo = Zoo('thing')
+
+# zoo.add_animal('tiger')
+# zoo.add_animal('toger')
+# zoo.add_animal('Bear')
+# zoo.add_animal('Ape')
+# zoo.add_animal('Baboon')
+# zoo.add_animal('Cougar')
+# zoo.add_animal('Eel')
+# zoo.add_animal('Emu')
+# zoo.add_animal('snake')
+
+# zoo.sell_animal('toger')
+
+# zoo.sort_animals()
+
+# zoo.get_animals()
 
 
 # daily ================================================================
 
+
 class Farm:
-    def __init__(self, ) -> None:
-        pass
+    def __init__(self, name) -> None:
+        self.name = name
+        self.animals_dict = {}
+
+    def add_animal(self, animal, number):
+
+        try:
+            self.animals_dict[animal] += number
+        except:
+            self.animals_dict[animal] = number
+
+        # if animal in self.animals_dict:
+        #     self.animals_dict[animal] = number
+        # else:
+        #     self.animals_dict[animal] += number
+
+    def get_info(self):
+        print(self.name)
+        for animal, amount in self.animals_dict.items():
+            print(f'{animal}: {amount}')
+        print('E-I-E-I-0!')
+
+
+    def get_animal_types(self):
+        return ', '.join(self.animals_dict.keys())
+
+    def get_short_info(self):
+        return f'McDonald’s farm has {self.get_animal_types()}'
+
+
+macdonald = Farm("McDonald")
+
+macdonald.add_animal('sheep', 5)
+macdonald.add_animal('cow', 14)
+macdonald.add_animal('sheep', 2)
+macdonald.add_animal('wolf', 5)
+
+print(macdonald.get_short_info())
