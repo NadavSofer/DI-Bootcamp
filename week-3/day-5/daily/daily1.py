77089a2669821b33baf3cba6b588def5e0f6ac47
NadavSofer <nadavsof5@gmail.com>
2023-04-11 18:01:20 +0300

    week-3 day-5 daily-2 full

--- week-3/day-5/daily/daily1.py
+++ week-3/day-5/daily/daily1.py
@@ -15,15 +15,4 @@ print(youtube_response)
 print(github_response)
 print(di_response)
 
-# Daily Challenge 2 part 1: OOP Quizz-------------------------------------
 
-# What is a class? ->  a class is a template for creating objects and methods.
-# What is an instance? -> an individual object of a class
-# What is encapsulation? -> defining methods and attributes in a single class
-# What is abstraction? ->  hiding unnecessary info from the user
-# What is inheritance? -> allow a class to get methods and properties from their parent class
-# What is multiple inheritance? -> a class with multiple parent classes
-# What is polymorphism? -> using the same function name for different things (that are usually similar)
-# What is method resolution order or MRO? -> the order of which python search for methods. form bottom up, from left to right
-
-# Daily Challenge 2 Part 2: Create A Deck Of Cards Class--------------------
