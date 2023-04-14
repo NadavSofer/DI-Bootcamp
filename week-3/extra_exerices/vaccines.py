5d6ad9a1f9b42d8608a1c1bff775c77517c1ec06
NadavSofer <nadavsof5@gmail.com>
2023-04-14 18:55:28 +0300

    week-3 extra vaccines

---
+++ week-3/extra_exerices/vaccines.py
@@ -0,0 +1,80 @@
+class Human:
+    def __init__(self, id_number:str, name:str, age:int, priority:bool, blood_type:str) -> None:
+        self.id_number = id_number
+        self.name = name
+        self.age = age
+        self.priority = priority
+        self.blood_type = blood_type
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.humans = []
+
+    def add_person(self, person: Human):
+        if type(person) == Human:
+            if person.priority == True or person.age > 60:
+                self.humans.insert(0, person)
+            else:
+                self.humans.append(person)
+        else:
+            raise ValueError('thing')
+
+    def remove_person(self):
+        self.humans.remove(self.humans[0])
+
+    def show_queue(self):
+        shown_queue = []
+        for person in self.humans:
+            shown_queue.append(person.name)
+        return shown_queue
+
+    def find_in_queue(self, person):
+        people_list = self.show_queue()
+        person_index = people_list.index(person)
+        return person_index + 1
+
+    def swap(self, person1, person2):
+        pass
+
+    def get_next(self):
+        if len(self.humans) > 0:
+            next_person = self.humans.pop(0)
+            return next_person.name
+        else:
+            return None
+
+    def get_next_blood_type(self, blood_type):
+        pass
+
+    def sort_by_age(self):
+        base = 0
+        # for person in self.humans:
+            # if person.age > 
+            
+
+
+steve = Human('45556673', 'Steve', 65, False, 'O')
+kate = Human('25544663', 'Kate', 32, False, 'A')
+brain = Human('25544663', 'Brain', 16, True, 'AB')
+josh = Human('25553685', 'Josh', 24, False, 'O')
+cake = 'cake'
+
+queue = Queue()
+
+queue.add_person(steve)
+queue.add_person(kate)
+queue.add_person(brain)
+queue.add_person(josh)
+# queue.add_person(cake)
+
+
+print(queue.show_queue())
+print(queue.find_in_queue('Steve'))
+print(queue.show_queue())
+
+
+# list1 = [1,2,3,4,5]
+# list1[-1] = 6
+
+# print(list1)
