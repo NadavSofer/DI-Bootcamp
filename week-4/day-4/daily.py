b1f1707c6a2ebcbc4c9db207aa38cdf839be1176
NadavSofer <nadavsof5@gmail.com>
2023-04-19 17:13:32 +0300

    week-4 day-4 daily

---
+++ week-4/day-4/daily.py
@@ -0,0 +1,26 @@
+import random
+import json
+import requests
+import psycopg2
+
+HOSTNAME = 'localhost'
+USERNAME = 'postgres'
+PASSWORD = 'Ns9517530809'
+DATABASE = 'REST_COUNTRIES'
+
+connection = psycopg2.connect(host = HOSTNAME, user = USERNAME, password = PASSWORD, dbname = DATABASE)
+
+cursor = connection.cursor()
+
+x = requests.get('https://restcountries.com/v3.1/all')
+
+country_list = x.json()
+
+for country in range(10):
+    random_num = random.randint(0, len(country_list)-1)
+    query = f"insert into countries (name, capital, flag, subregion, population) values ('{country_list[random_num]['name']['common']}', '{country_list[random_num]['capital'][0]}', '{country_list[random_num]['flag']}', '{country_list[random_num]['subregion']}', {country_list[random_num]['population']});"
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        connection.commit()
+    print ('item saved successfully')
+
