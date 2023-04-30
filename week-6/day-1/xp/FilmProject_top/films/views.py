4e680b42b3c0d78f4af41f6e15152eb6c1aa016d
NadavSofer <nadavsof5@gmail.com>
2023-04-30 14:34:01 +0300

    week-6 day-1 xp

---
+++ week-6/day-1/xp/FilmProject_top/films/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Film, Director
+from .forms import AddDirectorForm, AddFilmForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+class homepage(generic.ListView):
+    template_name = 'homepage.html'
+    model = Film
+    form_class = AddFilmForm
+    context_object_name = 'films'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['films'] = Film.objects.all()
+        return context
+
+
+class addFilm(generic.CreateView):
+    template_name = 'film/addFilm.html'
+    model = Film
+    form_class = AddFilmForm
+    success_url = reverse_lazy("homepage")
+
+class addDirector(generic.CreateView):
+    template_name = 'director/addDirector.html'
+    model = Director
+    form_class = AddDirectorForm
+    success_url = reverse_lazy("homepage")
