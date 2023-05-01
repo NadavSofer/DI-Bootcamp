460909f12f4accd564b1ad64237d0289e69b60ae
NadavSofer <nadavsof5@gmail.com>
2023-05-01 15:48:25 +0300

    week-6 day-2 daily

--- week-6/day-1/xp/FilmProject_top/accounts/forms.py
+++ week-6/day-1/xp/FilmProject_top/accounts/forms.py
@@ -2,7 +2,8 @@ from typing import Any
 from django import forms
 from .models import UserProfile
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser
+
 
 
 
@@ -15,5 +16,6 @@ class ProfileForm(forms.ModelForm):
 
 class SignupForm(UserCreationForm):
     class Meta:
-        model = User
-        fields  = ['username', 'first_name', 'last_name', 'password1', 'password2']
+        model = CustomUser
+        fields  = '__all__'
+
--- week-6/day-1/xp/FilmProject_top/accounts/models.py
+++ week-6/day-1/xp/FilmProject_top/accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from polls.models import Post
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser
+
+
 # Create your models here.
 class UserProfile(models.Model):
 
@@ -8,3 +12,16 @@ class UserProfile(models.Model):
 
     def __str__(self) -> str:
         return f'{self.user}'
+    
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+    def __str__(self):
+        return self.email
