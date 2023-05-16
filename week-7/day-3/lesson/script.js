6af7784f5d9cd8d213e208d825ebebf3ccbebeaa
NadavSofer <nadavsof5@gmail.com>
2023-05-16 12:27:32 +0300

    week-7 day-3 lesson-1 =

---
+++ week-7/day-3/lesson/script.js
@@ -0,0 +1,83 @@
+// console.log('hello');
+// let num = 1;
+// console.log(num);
+
+// let a;
+// let nul = null;
+// const to_change = [1,2,3,4,5];
+// let change = to_change;
+// change[0] = 0; // this is a way to change const, don't do that
+
+// let to_change_copy = [...to_change];// this is a copy of the list
+
+// let num_list = '123456789';
+// let leng = num_list.length;
+// let p_5 = num_list.indexOf('5');
+// console.log(leng);
+// console.log(p_5);
+
+// let word1 = 'thing';
+// let word2 = 'thing';
+// let words = `${word1} ${word2}`;
+// console.log(words);
+
+// -----------------------------------------------------------------------------------------
+
+// let addressNumber = '240';
+// let addressStreet = 'Hadas';
+// let country = 'Israel';
+// let globalAddress = `I live in ${addressStreet} ${addressNumber}, ${country}`;
+// console.log(globalAddress)
+
+// -----------------------------------------------------------------------------------------
+
+// let str = 'thing';
+// let num = '2';
+// let res = isNaN(num);
+// console.log(res);
+
+// let x = 10;
+// let str_x = x.toString();
+// console.log(typeof(str_x));
+
+// -----------------------------------------------------------------------------------------
+
+// alert('hello');
+// let res = prompt('please enter a number: ');
+// console.log(res);
+
+// let res = confirm('are ok?');
+// console.log(res);
+
+// -----------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5]; // they work like python
+// let arr_str = arr.toString();
+// let joined_arr = arr.join(' ');
+// let str_arr = joined_arr.split()
+
+// let exe_arr = ['cat','dog','fish','rabbit','cow']
+// console.log(exe_arr[1])
+// exe_arr.push('horse')
+// exe_arr1 = exe_arr.splice(3, 1)
+// console.log(exe_arr)
+// -----------------------------------------------------------------------------------------
+
+// let obj = {thing: '5', arr: [1,2,3,4,5]};
+
+// obj.thing = 12
+// obj.stuff = 'stuff'
+// obj.f = function(){
+//     alert('hello')
+//     return 'thing'
+// }
+
+// delete obj.stuff
+
+// console.log(obj.arr[1]);
+// console.log(obj.f());
+
+// let thing = {username: 'thing', password:123456};
+// let database = [thing]
+// let newsfeed = [{username: 'thing'}, {password:123456} , {timeline: 'what'}]
+// console.log(newsfeed);
