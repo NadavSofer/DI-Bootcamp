623bacd7c2bcd9cf1ba4d62b4f73a8aa3f159a37
NadavSofer <nadavsof5@gmail.com>
2023-05-16 15:32:21 +0300

    week-7 day-3 daily2

---
+++ week-7/day-3/exerices/daily/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+// Daily Challenge2: Stars -------------------------------------------------
+
+// for (let i = 0; i < 6; i++) {
+//     let row = ''
+//     for (let j = 0; j <= i ; j++) {
+//         row += '*'
+//     }
+//     console.log(row)
+// };
