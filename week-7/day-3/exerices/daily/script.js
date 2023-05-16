7dd64e671640441c246d3f4606c061260f2c3173
NadavSofer <nadavsof5@gmail.com>
2023-05-16 16:06:03 +0300

    week-7 day-3 daily1

--- week-7/day-3/exerices/daily/script.js
+++ week-7/day-3/exerices/daily/script.js
@@ -1,6 +1,17 @@
+// Daily Challenge: Not Bad -------------------------------------------------
 
+let sentence = 'The movie is not that bad, I like it'
+let wordNot = sentence.search('not');
+let wordBad  = sentence.search('bad');
 
+console.log(wordNot)
+console.log(wordBad)
 
+if (wordNot < wordBad){
+    let res1 = sentence.slice(wordNot, wordBad+3)
+    let out = sentence.replace(res1, 'good')
+    console.log(out)
+}
 
 // Daily Challenge2: Stars -------------------------------------------------
 
@@ -11,3 +22,11 @@
 //     }
 //     console.log(row)
 // };
+
+// VVV this is just a second answer VVV
+
+// let row = ''
+// for (let i = 0; i < 6; i++){
+//     row = '*'.repeat(i+1)
+//     console.log(row)
+// }
