587976534f25e49a72ef663fd5450cd2a69d7521
NadavSofer <nadavsof5@gmail.com>
2023-05-18 13:01:01 +0300

    week-7 day-5 daliy and gold

---
+++ week-7/day-5/daliy/script.js
@@ -0,0 +1,15 @@
+let input = prompt('number ')
+let counter = 0
+for (let i = Number(input); i >= 0; i = i - counter) {
+    
+    if (i <= 0 || i-counter < 0){
+        console.log(`${i} bottles of beer on the wall \n${i} bottles of beer \nTake ${counter+1} down, pass them around \nno more bottles of beer on the wall`);
+    }
+    else if (counter === 0){
+        console.log(`${i} bottles of beer on the wall \n${i} bottles of beer \nTake ${counter+1} down, pass it around \n${i-counter-1} bottles of beer on the wall`);
+    }
+    else{
+        console.log(`${i} bottles of beer on the wall \n${i} bottles of beer \nTake ${counter+1} down, pass them around \n${i-counter-1} bottles of beer on the wall`);
+    }
+    counter++
+};
