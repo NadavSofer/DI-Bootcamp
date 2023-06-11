424f6628a1de4943014de3d97cef7c5327c3c575
NadavSofer <nadavsof5@gmail.com>
2023-06-11 15:01:00 +0300

    week-11 day-1 daily

---
+++ week-11/day-1/mandatory/daily/src/components/ImgCarousel.js
@@ -0,0 +1,25 @@
+import '../ImgCarousel.css'
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+const ImgCarousel = () => {
+    return (
+        <Carousel infiniteLoop autoPlay>
+            <div>
+                <img src="/baby_panda.png" alt='baby panda'/>
+                <p className="legend">baby panda</p>
+            </div>
+            <div>
+                <img src="kiwi_in_the_ocean.png" alt='kiwi in the ocean'/>
+                <p className="legend">kiwi in the ocean</p>
+            </div>
+            <div>
+                <img src="mountain_with_foxes.png" alt='mountain with foxes'/>
+                <p className="legend">mountain with foxes</p>
+            </div>
+        </Carousel>
+    );
+}
+
+export default ImgCarousel
