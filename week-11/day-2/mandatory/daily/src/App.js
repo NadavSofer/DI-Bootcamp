ff0176623a2392fb597cc09fb9cb0745bbfbff83
NadavSofer <nadavsof5@gmail.com>
2023-06-12 16:02:46 +0300

    week-11 day-2 daily

--- week-11/day-2/mandatory/daily/src/App.js
+++ week-11/day-2/mandatory/daily/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
 
 function App() {
+  const [languages, setLanguages] = useState([        
+  {name: "Php", votes: 0},
+  {name: "Python", votes: 0},
+  {name: "JavaScript", votes: 0},
+  {name: "Java", votes: 0}])
+
+const addVote = (item) => {
+  console.log(item);
+  item['votes']++
+  setLanguages([...languages])
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='language-container'>
+          {
+            languages.map(item=> {
+              return (<div>
+                <h2>{item['name']}</h2>
+                <h2>{item['votes']}</h2>
+                <button onClick={(e)=> addVote(item)} >vote</button>
+              </div>)
+            })
+          }
+        </div>
+
       </header>
     </div>
   );
