aa722c80dff9acb44612b395dd636a60c076cb7e
NadavSofer <nadavsof5@gmail.com>
2023-06-14 17:02:36 +0300

    week-11 day-4 fix

--- week-11/day-4/mandatory/daliy/server/server.js
+++ week-11/day-4/mandatory/daliy/server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 app.get('/api/hello',  (req, res)=>{
     res.json('Hello From Express')
+})
+
+app.post('/api/world',  (req, res)=>{
+    res.json(`I received your POST request. This is what you sent me: ${req.body}`)
 })
 
 app.listen(3030, ()=>{
